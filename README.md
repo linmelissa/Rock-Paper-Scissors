682cf68e957e02278c9b7273d6f8e18b5c5ae277
Melissa Lin <linmel0040@gmail.com>
2023-02-02 02:56:01 -0600

    sprout lands edition

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Rock Paper Scissors
+# Rock Paper Scissors: Sprout Lands Edition
 
 ## Description
-Rock paper scissors game against a computer.
+Rock paper scissors game created with the Sprout Lands assets by Cup Nooble.
+
+## Instructions
+- First to five points wins
+- Rock beats Axe
+- Axe beats Grass
+- Grass beats Rock
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection = "";
+let results = "";
 
 // randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’
 function getComputerChoice() {
@@ -20,69 +22,119 @@ function getComputerChoice() {
 // plays a single round of Rock Paper Scissors
 // return string that declares winner of round
 function playRound(playerSelection, computerSelection) {
-    // makes answer case insensitive 
-    playerSelection = playerSelection.toLowerCase();
-
     if (playerSelection == "rock" && computerSelection == "Rock") {
-        return "You drew!";
+        return "You drew! You both played Rock.";
     }
     else if (playerSelection == "rock" && computerSelection == "Paper") {
         computerScore++;
-        return "You lose! Rock loses to Paper.";
+        return "You lose! Rock loses to Grass.";
     }
     else if (playerSelection == "rock" && computerSelection == "Scissors") {
         playerScore++;
-        return "You win! Rock beats Scissors.";
+        return "You win! Rock beats Axe.";
     }
     else if (playerSelection == "paper" && computerSelection == "Rock") {
         playerScore++;
-        return "You win! Paper beats Rock.";
+        return "You win! Grass beats Rock.";
     }
     else if (playerSelection == "paper" && computerSelection == "Paper") {
-        return "You drew!";
+        return "You drew! You both played Grass";
     }
     else if (playerSelection == "paper" && computerSelection == "Scissors") {
         computerScore++;
-        return "You lose! Paper loses to Scissors.";
+        return "You lose! Grass loses to Axe.";
     }
     else if (playerSelection == "scissors" && computerSelection == "Rock") {
         computerScore++;
-        return "You lose! Scissors loses to Rock.";
+        return "You lose! Axe loses to Rock.";
     }
     else if (playerSelection == "scissors" && computerSelection == "Paper") {
         playerScore++;
-        return "You win! Scissors beats Paper."
+        return "You win! Axe beats Grass."
     }
     else if (playerSelection == "scissors" && computerSelection == "Scissors") {
-        return "You drew!"
-    }
-    else {
-        return "Please choose only 'Rock' 'Paper' or 'Scissors'.";
+        return "You drew! You both played Axe."
     }
 }
+
+
+// tools buttons
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const information = document.getElementById('information');
+
+// play again button
+const playAgainButton = document.createElement('button');
+const text = document.createTextNode("play again");
+
+// event listeners
+rockButton.addEventListener("click", rockPressed);
+paperButton.addEventListener("click", paperPressed);
+scissorsButton.addEventListener("click", scissorsPressed);
+playAgainButton.addEventListener("click", playAgainPressed);
+
+
+// functions
+// rock button pressed
+function rockPressed() {
+    playerSelection = "rock";
+    information.innerText = playRound(playerSelection, getComputerChoice()) + 
+    "\nYou: " + playerScore + "\n Sprout Bunny: " + computerScore;
+    checkScore();
+};  
+
+// paper button pressed
+function paperPressed() {
+    playerSelection = "paper";
+    information.innerText = playRound(playerSelection, getComputerChoice()) + 
+    "\nYou: " + playerScore + "\n Sprout Bunny: " + computerScore;
+    checkScore();
+};  
+
+// scissors button pressed
+function scissorsPressed() {
+    playerSelection = "scissors";
+    information.innerText = playRound(playerSelection, getComputerChoice()) + 
+    "\nYou: " + playerScore + "\n Sprout Bunny: " + computerScore;
+    checkScore();
+};  
+
+function checkScore() {
+    if (playerScore > 4) {
+        disableButtons();
+        information.innerText = "You beat Sprout Bunny! :D" + 
+        "\nYou: " + playerScore + "\n Sprout Bunny: " + computerScore +
+        "\n\n";    
+        playAgainButton.appendChild(text);
+        information.appendChild(playAgainButton);
+    }
     
-// plays rock paper scissors for 5 rounds
-// displays score at the end
-// reports a winner or loser at the end
-function game() {
-    let playerSelection = ""; 
-    let computerSelection = ""; 
-    let results = "";
-    
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Please choose Rock, Paper, or Scissors.");
-        computerSelection = getComputerChoice();
-        results = playRound(playerSelection, computerSelection);
-        console.log(results);
-        console.log("Match Results: Player:" + playerScore + " Computer: " + computerScore);
+    if (computerScore > 4) {
+        disableButtons();
+        information.innerText = "You lost to Sprout Bunny! D:" + 
+        "\nYou: " + playerScore + "\n Sprout Bunny: " + computerScore +
+        "\n\n";
+        playAgainButton.appendChild(text);
+        information.appendChild(playAgainButton);
     }
-    if (computerScore > playerScore) {
-        console.log("You lost to a computer...");
-    }
-    else if (playerScore > computerScore) {
-        console.log("Wow you beat a computer!");
-    }
-    else {
-        console.log("Nobody won.");
-    }
+}
+
+function playAgainPressed(){
+    playerScore = 0;
+    computerScore = 0; 
+    information.innerText = "Choose a tool to begin playing!"
+    enableButtons();
+}
+
+function enableButtons() {
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+}
+
+function disableButtons() {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
 }
